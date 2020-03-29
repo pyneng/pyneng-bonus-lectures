e1a390c82f6cd6e5d305431ccec74bd0af96697b
pyneng <pyneng.course@gmail.com>
2020-03-29 13:20:46 +0000

    code to reformat

---
+++ examples/04_black/raw_code_3.py
@@ -0,0 +1,22 @@
+import telnetlib
+import time
+
+
+class CiscoTelnet:
+    def __init__(self, ip, username, password, enable_password=None,
+                 disable_paging=True):
+        self.ip = ip
+        self.telnet = telnetlib.Telnet(ip)
+        self.telnet.read_until(b"Username:")
+        self.telnet.write(username.encode("ascii") + b"\n")
+
+        self.telnet.read_until(b"Password:")
+        self.telnet.write(password.encode("ascii") + b"\n")
+        if enable_password:
+            self.telnet.write(b"enable\n")
+            self.telnet.read_until(b"Password:")
+            self.telnet.write(enable_password.encode("ascii") + b"\n")
+        if disable_paging:
+            self.telnet.write(b"terminal length 0\n")
+        time.sleep(0.5)
+        self.telnet.read_very_eager()
