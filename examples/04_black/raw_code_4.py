d1158e85e2cb473ed60367c8ebd8ee2723fa2fc4
pyneng <pyneng.course@gmail.com>
2020-03-29 15:08:56 +0000

    code to reformat

--- examples/04_black/raw_code_4.py
+++ examples/04_black/raw_code_4.py
@@ -1,2 +1,7 @@
 def fun(a, b):
-    print(a,b)
+	print(a,b)
+
+def example(session):
+	result = (
+		session.query(models.Customer.id).filter(models.Customer.account_id == account_id, models.Customer.email == email_address).order_by(models.Customer.id.asc()).all()
+	)
